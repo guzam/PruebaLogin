da7e0f0e1efff348530dabbeda791c234abad59f
Federico Balbuena <federico.balbuena@gmail.com>
2022-09-30 16:55:07 -0300

    Agrego modal en "¿Olvido su contraseña?"

--- loginCarreraDemente/src/app/home/home.page.ts
+++ loginCarreraDemente/src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  /* constructor() {} */
+  @ViewChild(IonModal) modal: IonModal;
 
+  message = '';
+  respuesta: string;
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.respuesta, 'confirm');
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Hello, ${ev.detail.data}!`;
+    }
+  }
 }
